b1920f9fb581b4425dd0dc8f6e3e1152c28d7c65
marcosraach.1@gmail.com <marcosraach.1@gmail.com>
2023-08-12 19:50:48 -0300

    second commit

--- src/screens/Hamper.js
+++ src/screens/Hamper.js
@@ -3,24 +3,24 @@ import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 
 import top from "../../assets/top.png"
 import logo from "../../assets/logo.png"
-import {Lato_400Regular, Lato_700Bold} from "@expo-google-fonts/lato";
+import { Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato";
 
 const width = Dimensions.get("screen").width;
 
 
 export default function Hamper() {
     return <>
-        <Image source = { top } style = { style.top } />
-        <Text style = { style.title } >Hamper Details</Text>
+        <Image source={ top } style={ style.top } />
+        <Text style={ style.title } >Hamper Details</Text>
 
-        <View style = { style.hamper }>
-            <Text style = { style.name } >Hamper </Text>
-            <View style = { style.farm }>
-                <Image source = { logo } style = { style.farmImage } />
-                <Text style = { style.farm } >Marcos` FARM </Text>
+        <View style={ style.hamper }>
+            <Text style={ style.name } >Hamper </Text>
+            <View style={ style.farm }>
+                <Image source={ logo } style={style.farmImage} />
+                <Text style={ style.farm } >Marcos` FARM </Text>
             </View>
-            <Text style = { style.description } >First Class Products by Marquinho </Text>
-            <Text style = { style.price } >PRICE: R$ 40 </Text>
+            <Text style={ style.description } >First Class Products by Marquinho </Text>
+            <Text style={ style.price } >PRICE: R$ 40 </Text>
         </View>
     </>
 }
@@ -34,12 +34,12 @@ const style = StyleSheet.create({
         width: "100%",
         position: "absolute",
         textAlign: "center",
+        color: "white",
         fontSize: 16,
         lineHeight: 8,
-        color: "white",
         fontWeight: "bold",
-        padding: 16,
         marginTop: 60,
+        padding: 16,
     },
     hamper: {
         paddingVertical: 8,
@@ -52,18 +52,18 @@ const style = StyleSheet.create({
         fontFamily: "Lato_700Bold"
     },
     farm: {
-      flexDirection: "row",
-      paddingVertical: 12,
+        paddingVertical: 12,
+        flexDirection: "row",
     },
     farmImage: {
         width: 32,
         height: 32,
     },
     farmName: {
-      fontSize: 16,
-      lineHeight:26,
-      marginLeft: 12,
-      fontFamily: Lato_400Regular,
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: Lato_400Regular,
     },
     description: {
         color: "#A3A3A3",
