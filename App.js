a26683a9ccf2f928dff20d250007dcdd62e21c15
Marcos <marcosraach.1@gmail.com>
2023-08-21 14:30:39 -0300

    fim do projeto

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import {StatusBar, SafeAreaView, View} from 'react-native';
-import Hamper from './src/screens/Hamper';
-import { useFonts, Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato";
-import mock from "./src/mocks/hamper";
-import * as SplashScreen from 'expo-splash-screen';
-
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
-    const [loadedFonts] = useFonts([
-        Lato_400Regular, Lato_700Bold,
-    ]);
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
 
-    if (!loadedFonts) {
-        return <SplashScreen/>
-    }
-    return (
-        <SafeAreaView>
-            <StatusBar/>
-            <Hamper {...mock} />
-        </SafeAreaView>
-    );
+  if (!fonteCarregada) {
+    return <AppLoading />
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar />
+      <Cesta {...mock} />
+    </SafeAreaView>
+  );
 }
